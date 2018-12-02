36252248d58703314e22e5448c111f236b913007
shibbo <xarf98@yahoo.com>
2018-12-02 14:18:02 -0500

    add in agl's DisplayList class and GPU memory address class

---
+++ agl/DisplayList.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "types.h"
+#include "GPUMemAddr.h"
+#include "sead/Heap.h"
+
+namespace agl
+{
+    class DisplayList
+    {
+        public:
+        DisplayList();
+
+        virtual ~DisplayList();
+
+        void setControlMemeory(void *, s32);
+        void clear();
+        void setBuffer(agl::GPUMemAddr<u8>, u64);
+        void setValidSize(u64 size);
+        void copyTo(agl::DisplayList *) const;
+        void beginDisplayList();
+        void endDisplayList();
+        bool beginDisplayListBuffer(agl::GPUMemAddr<u8>, u64, bool);
+        void endDisplayListBuffer(sead::Heap *);
+        void adjustValueSize();
+        void invalidateCPUCache() const;
+        void dump() const;
+        bool suspend(void **);
+        void resume(void *, u64);
+        u64 calcRemainingSize();
+
+        u64 _8;
+        u64 _10;
+        u64 _18;
+        u32 mUsedSize; // _20
+        u32 _24;
+        u64 mSize; // _28
+    };
+};
---
+++ agl/GPUMemAddr.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "types.h"
+
+namespace agl
+{
+    template<typename T>
+    class GPUMemAddr
+    {
+        u64* mPtr; // _0
+        u64 _8;
+    };
+};
