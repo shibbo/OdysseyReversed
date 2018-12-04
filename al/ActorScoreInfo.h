12df91584c943368e79506b3758873c19f75a71f
shibbo <xarf98@yahoo.com>
2018-12-04 16:09:26 -0500

    Add in ActorScoreInfo class.

---
+++ al/ActorScoreInfo.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace al
+{
+    class ByamlIter;
+
+    class ActorScoreInfo
+    {
+        public:
+        ActorScoreInfo();
+        
+        void init(al::ByamlIter const &);
+
+        char** mFactorName; // _0
+        char** mCategoryName; // _8
+    };
+};
