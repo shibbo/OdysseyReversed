47091035b8b82fe96269d1c507f9bbba02cda71d
shibbo <xarf98@yahoo.com>
2018-12-11 11:41:17 -0500

    ModelKeeper and Actor Resources.

--- al/LiveActor.h
+++ al/LiveActor.h
@@ -8,6 +8,7 @@
 #include "keeper/AudioKeeper.h"
 #include "keeper/EffectKeeper.h"
 #include "keeper/HitSensorKeeper.h"
+#include "keeper/ModelKeeper.h"
 #include "keeper/NerveKeeper.h"
 #include "keeper/ShadowKeeper.h"
 #include "keeper/StageSwitchKeeper.h"
@@ -38,7 +39,7 @@ namespace al
         al::ActorScoreKeeper* mScoreKeeper; // _70
         al::Collider* mCollider; // _78
         u64* _80; // unknown
-        u64* _88; // ModelKeeper*
+        al::ModelKeeper* mModelKeeper; // _88
         al::NerveKeeper* mNerveKeeper; // _90
         al::HitSensorKeeper* mHitSensorKeeper; // _98
         u64* _A0; // ScreenPointKeeper*
---
+++ al/keeper/ModelKeeper.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "types.h"
+#include "al/resource.h"
+#include "sead/matrix.h"
+#include "sead/vector.h"
+
+namespace al
+{
+    class ModelKeeper
+    {
+        public:
+        ModelKeeper();
+
+        virtual ~ModelKeeper();
+
+        void initResource(al::ActorResource const *);
+        void createMatAnimForProgram(s32);
+        u64* getAnimResource() const;
+        u64* getModelResource() const;
+        void setDisplayRootJointMtxPtr(sead::Matrix34<f32> const *);
+        void setModelLodCtrl(u64 *); // al::ModelCtrl*
+
+        void show();
+        void hide();
+        void update();
+        void updateLast();
+        void calc(sead::Matrix34<f32> const &, sead::Vector3<f32> const &);
+        sead::Matrix34<f32>* getBaseMtx() const;
+        sead::Matrix34<f32>* getWorldMtxPtrByIndex(s32) const;
+
+        u64* _8; // maybe sead::SafeStringBase<char>*
+        u64* _10; // al::ModelCtrl*
+        al::ActorResource* mResource; // _18
+        u64* _20; // al::AnimPlayerSkl*
+        u64* _28; // al::AnimPlayerSimple*
+        u64* _30; // al::AnimPlayerSimple*
+        u64* _38; // al::AnimPlayerSimple*
+        u64* _40; // al::AnimPlayerSimple*
+        u64* _48; // al::AnimPlayerSimple*
+        u64* _50; // al::AnimPlayerSimple*
+        u64* _58;
+        u8 _60;
+        u8 _61;
+        u8 _62;
+        u8 _63;
+        u32 _64;
+    };
+};
