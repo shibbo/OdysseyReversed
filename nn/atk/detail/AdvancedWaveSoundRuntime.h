7161ec1d85b6597b220baa2f7b7b83be195ece61
shibbo <xarf98@yahoo.com>
2018-12-21 14:55:31 -0500

    SoundArchivePlayer and its contained classes.

---
+++ nn/atk/detail/AdvancedWaveSoundRuntime.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "types.h"
+
+namespace nn
+{
+    namespace atk
+    {
+        namespace detail
+        {
+            class AdvancedWaveSoundRuntime
+            {
+                public:
+                AdvancedWaveSoundRuntime();
+                ~AdvancedWaveSoundRuntime();
+
+                void Initialize(s32, void **, void const *);
+                void Finalize();
+                s32 GetActiveCount() const;
+                void SetupUserParam(void **, u64);
+                void Update();
+
+                u8 _0[0x30];
+            };
+        };
+    };
+};
