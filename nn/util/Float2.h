331d127fb8bd11ee82a6b09cc7bb32258494294a
shibbo <xarf98@yahoo.com>
2018-12-28 00:12:01 -0500

    Start with some layout classes.

---
+++ nn/util/Float2.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "types.h"
+
+namespace nn
+{
+    namespace util
+    {
+        struct Float2
+        {
+            u64 _0;
+            u64 _8;
+            u64 _10;
+            u64 _18;
+        };
+    };
+};
