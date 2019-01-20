3f548304972a860a6cc88f9939cdac398253d4eb
shibbo <xarf98@yahoo.com>
2019-01-19 21:54:03 -0500

    a whole bunch of nn::nex and crypto stuff

---
+++ nn/nex/data.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "RootObject.h"
+
+namespace nn
+{
+    namespace nex
+    {
+        class Data : public nn::nex::RootObject
+        {
+        public:
+            Data();
+
+            virtual ~Data();
+
+            u8 _8;
+        };
+    };
+};
