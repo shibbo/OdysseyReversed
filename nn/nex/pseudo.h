ff77db47af198e9fff6e4d7abdd2f8b03e4d9aaf
shibbo <xarf98@yahoo.com>
2019-02-01 10:23:08 -0500

    Implement a lot of NEX protocol functionality, and fix a lot of psuedo stuff.

--- nn/nex/pseudo.h
+++ nn/nex/pseudo.h
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include "instance.h"
 #include "RootObject.h"
 #include "types.h"
 
@@ -12,6 +13,8 @@ namespace nn
 {
     namespace nex
     {
+        class PseudoGlobalVariableList;
+
         class PseudoGlobalVariableRoot : public nn::nex::RootObject
         {
         public:
@@ -19,23 +22,66 @@ namespace nn
 
             virtual ~PseudoGlobalVariableRoot();
 
-            void ResetContextForAllVariables(u32);
-            void AllocateExtraContextsForAllVariables(u64);
-            void FreeExtraContextsForAllVariables();
-            s64 GetNbOfExtraContexts();
+            virtual void AllocateExtraContexts() = 0;
+            virtual void FreeExtraContexts() = 0;
+            virtual void ResetContext(u32) = 0;
+            virtual PseudoGlobalVariableRoot* GetNext() = 0;
+            virtual void SetNext(PseudoGlobalVariableRoot* pNextVariable) = 0;
 
-            u64 _8;
+            static void ResetContextForAllVariables(u32);
+            static void AllocateExtraContextsForAllVariables(u64);
+            static void FreeExtraContextsForAllVariables();
+            static s64 GetNbOfExtraContexts();
+
+            nn::nex::PseudoGlobalVariableRoot* mNextRoot; // _8
 
             static s64 s_uiNbOfExtraContexts;
+            static PseudoGlobalVariableList s_oList;
         };
 
         class PseudoGlobalVariableList : public nn::nex::RootObject
         {
         public:
-            static u64* s_pVariableListHead;
+            PseudoGlobalVariableList();
+
+            virtual ~PseudoGlobalVariableList();
+
+            void AddVariable(nn::nex::PseudoGlobalVariableRoot *);
+            void RemoveVariable(nn::nex::PseudoGlobalVariableRoot *);
+            static nn::nex::PseudoGlobalVariableRoot* GetVariable(u32 idx);
+            static u32 FindVariableIndex(nn::nex::PseudoGlobalVariableRoot *);
+            void AllocateExtraContextsForAllVariables();
+            void FreeExtraContextsForAllVariables();
+            void ResetContextForAllVariables(u32);
+            static u32 GetNbOfVariables();
+
+            static PseudoGlobalVariableRoot* s_pVariableListHead;
+            static u32 m_uiNbOfVariables;
         };
 
         template<typename T>
-        class PseudoGlobalVariable : public nn::nex::RootObject { };
+        class PseudoGlobalVariable : public nn::nex::PseudoGlobalVariableRoot 
+        { 
+        public:
+            PseudoGlobalVariable();
+            
+            virtual ~PseudoGlobalVariable();
+
+            virtual void AllocateExtraContexts();
+            virtual void FreeExtraContexts();
+            virtual void ResetContext(u32);
+            virtual PseudoGlobalVariableRoot* GetNext();
+            virtual void SetNext(PseudoGlobalVariableRoot* pNextVariable);
+        };
+
+        class PseudoSingleton : public nn::nex::InstanceControl
+        {
+        public:
+            PseudoSingleton(u32);
+
+            virtual ~PseudoSingleton();
+
+            static bool s_bUseInstantiationContext;
+        };
     };
 };
