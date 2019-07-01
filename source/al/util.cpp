6de0601e7d0477310e1ac2cdd28a435a90cdf0e6
shibbo <xarf98@yahoo.com>
2019-07-01 16:32:52 -0400

    More function addresses defined for linker

---
+++ source/al/util.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file util.cpp
+ * @brief Functions restored from version 1.2 from the al library
+ */
+
+#include "al/util.h"
+#include "types.h"
+
+namespace al
+{
+    // put functions here
+};
