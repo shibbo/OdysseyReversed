eb9148abf70298d49c680fc54599d8e01278cbe8
shibbo <jtandrew@umes.edu>
2019-07-06 00:05:56 -0400

    a few misc fixes for stability

--- build.py
+++ build.py
@@ -44,7 +44,7 @@ def command_available(cmd):
 		return False
 		
 def main(argc, argv):
-	required_commands = ['make', 'aarch64-none-elf-g++', 'aarch64-none-elf-objdump', 'python']
+	required_commands = ['make', 'aarch64-none-elf-objdump', 'python']
 
 	for cmd in required_commands:
 		if command_available(cmd) == False:
@@ -54,22 +54,6 @@ def main(argc, argv):
 	
 	if 'make' in argv:
 		run_proc('make')
-		
-		print("Correcting incorrect MOD0...")
-		
-		subprocess.run("wine tools/nsnsotool.exe OdysseyReversed.nso", shell=True)
-
-		with open("OdysseyReversed.nso", 'r+b') as nso_file:
-			nso = nso_file.read()
-			mod0_offset = nso.find(b'MOD0')
-			nso_file.seek(mod0_offset - 8)
-			nso_file.seek(mod0_offset + 8)
-			bss_start = nso_file.read(4)
-			nso_file.seek(mod0_offset + 0x24)
-			nso_file.write(bss_start)
-			nso_file.close()
-			
-		print("Corrected.")
 
 		os.rename("OdysseyReversed.nso", "subsdk1")
 		
